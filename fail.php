fe2a65af2974ab0132bc543ffe230d1cd2653fda
kihajunn <kihajunn214@yahoo.ne.jp>
2022-03-20 05:36:13 +0900

    first commit

---
+++ fail.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<title>課題20</title>
+</head>
+<body>
+<a href="form.php">計算フォームはこちら</a>
+</body>
+</html>
---
+++ form.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<title>課題20</title>
+</head>
+<body>
+<h1>足し算フォーム</h1>
+<form method="get" action="calculate.php">
+<p>
+<label for="text1">入力フォーム1</label>
+<input type="number" name="data1" id="text1" placeholder="数字を入力してください">
+</p>
+<p>
+<label for="text2">入力フォーム2</label>
+<input type="number" name="data2" id="text2" placeholder="数字を入力してください">
+</p>
+<input type="submit" name="submit" value="送信">
+</form>
+</body>
+</html>
